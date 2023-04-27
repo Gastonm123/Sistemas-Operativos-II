3b1377916eb46dfdab0151580dcc741d92972cf2
SebastianMestre <mestresebastianunr@gmail.com>
2023-04-26 21:34:48 -0300

    Implementa escenario de productor y consumidor

--- Entrega2/code/threads/thread_test_prod_cons.cc
+++ Entrega2/code/threads/thread_test_prod_cons.cc
@@ -6,12 +6,49 @@
 
 
 #include "thread_test_prod_cons.hh"
+#include "system.hh"
+#include "channel.hh"
 
 #include <stdio.h>
 
+static const unsigned NUM_ITEMS = 10;
+
+Channel* chan;
+
+void
+Producer(void*)
+{
+	for (int i = 0; i < NUM_ITEMS; ++i) {
+		// random-looking numbers for testing purposes
+		int message = (i * i) % 23;
+		chan->Send(message);
+	}
+	printf("Producer finished.\n");
+}
+
+void
+Consumer(void*)
+{
+	for (int i = 0; i < NUM_ITEMS; ++i) {
+		int message;
+		chan->Receive(&message);
+		printf("Consumer received message %d\n", message);
+	}
+	printf("Consumer finished.\n");
+}
 
 void
 ThreadTestProdCons()
 {
-    printf("Test unimplemented!\n");
+	chan = new Channel("prod_cons channel");
+	Thread* producer = new Thread("producer");
+	Thread* consumer = new Thread("consumer");
+
+	producer->Fork(Producer, nullptr);
+	consumer->Fork(Consumer, nullptr);
+
+	producer->Join();
+	consumer->Join();
+
+	delete chan;
 }
